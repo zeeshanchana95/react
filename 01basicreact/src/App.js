3c71f45c534b4a8a086c95452aa71bdcca5a6766
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-08 15:33:42 +0500

    create react projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chaie or React | Zeeshan Ahmed</h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chaie or Vite | Zeeshan Ahmed</h1>;
+}
+
+export default App;
