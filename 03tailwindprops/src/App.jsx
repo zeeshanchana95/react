7c5330b986a61351d16ccd59c71af36ecfcf05d4
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-12 14:32:25 +0500

    add tailwind css and props in react proejct UPDATED

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
+  // let myObj = { username: "zeeshan", age: 21 };
+  // let newArr = [1, 2, 3];
   return (
     <>
-      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind Test</h1>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind Test
+      </h1>
+      <Card username="chaie or code" btnText="click me" />
+      <Card username="hitesh" btnText="visit me" />
     </>
   );
 }
