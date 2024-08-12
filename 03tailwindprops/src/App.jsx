5d3270754436d9bb41d0dd985b3a7dffb11c3962
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-12 14:13:18 +0500

    add tailwind css and props in react proejct

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind Test</h1>
+    </>
+  );
+}
+
+export default App;
