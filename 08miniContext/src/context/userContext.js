ae3d105da9ab24204be97fc2b9738919c22cfb88
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-08-18 20:36:45 +0500

    react useContext part 1

---
+++ 08miniContext/src/context/userContext.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+//part-1: creating and exporting context
+const UserContext = React.createContext(); //context will give context provider
+
+export default UserContext;
